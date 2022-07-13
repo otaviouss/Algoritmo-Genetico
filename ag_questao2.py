3af1fe1bd399bc721d37b7926e6c2b43aab541d0
Otavio S. Gomes <otaviosantosg@gmail.com>
2022-07-12 21:53:12 -0300

    melhorias gerais

--- ag_questao2.py
+++ ag_questao2.py
@@ -1,7 +1,5 @@
 import math
 import random
-import csv
-from time import sleep
 
 def funcao(p_i, p_i_min, parametros_obj):       
     return (parametros_obj[0] * (p_i**2)) + (parametros_obj[1] * p_i) + parametros_obj[2] + abs(parametros_obj[3] * math.sin(parametros_obj[4] * (p_i_min - p_i)))
@@ -11,9 +9,7 @@ def calcular_funcao_individuo(individuo, min, dados_objetivo):
 
     for i in range(len(individuo)):
         parametros_obj = [dados_objetivo[0][i], dados_objetivo[1][i], dados_objetivo[2][i], dados_objetivo[3][i], dados_objetivo[4][i]]
-        #print("PArametos: ", parametros_obj)
         soma += funcao(individuo[i], min[i], parametros_obj)
-        #print(soma)
     
     return soma
 
@@ -60,30 +56,26 @@ def inicializacao(tam_pop, min, max, dados_objetivo):
     for i in range(tam_pop):
         montante = 10500 - sum(min)
 
-        individuo = min
+        individuo = min.copy()
         for j in range(40):
-            p_i = random.uniform(min[j], (max[j] - min[j]))
+            p_i = random.uniform((max[j] - min[j])*0.6, (max[j] - min[j]))
+            if(montante-p_i < 0):
+                individuo[j] += (montante)
+                break
             individuo[j] += p_i
             montante -= p_i
-            if montante <= 100:
-                break
-            
-        print("SOMA: ", sum(individuo), " VALOR FO: ", calcular_funcao_individuo(individuo, min, dados_objetivo))
-        sleep(1)
-            
+
         vi.append(individuo)
 
     return vi
 
 def selecao_torneio(pop, t_size, min, dados_objetivo):
     pop_size = len(pop)
-    #contestant = [[]]*t_size
     best = pop[random.randint(0, pop_size-1)]
 
     contestant = best
     for j in range(1, t_size):
         contestant = pop[random.randint(0, pop_size-1)]
-        #print("Calculo: ", calcular_funcao_individuo(contestant, min, dados_objetivo))
         if(calcular_funcao_individuo(contestant, min, dados_objetivo) < calcular_funcao_individuo(best, min, dados_objetivo)):
             best = contestant
 
@@ -92,62 +84,49 @@ def selecao_torneio(pop, t_size, min, dados_objetivo):
 def ajustar_ordem(a, b):
     return (a,b) if (a>b) else (b,a)
 
-def cruzamento_flat(pais):
+def cruzamento_simples(pais):
     filhos = []
 
     # Número ímpar vai dar um número diferente de filhos
     for i in range(math.floor(len(pais)/2)):
 
-        filho = []
-        for j in range(40):
-            p_i = ajustar_ordem(pais[i][j], pais[len(pais)-i-1][j])
-            filho.append( random.uniform(p_i[0], p_i[1]) )
- 
-        filhos.append(filho)
+        filho1 = pais[i].copy()
+        filho2 = pais[i].copy()
+        filho3 = pais[i].copy()
+        filho4 = pais[i].copy()
 
-        filho = []
-        for j in range(40):
-            p_i = ajustar_ordem(pais[i][j], pais[len(pais)-i-1][j])
-            filho.append( random.uniform(p_i[0], p_i[1]) )
+        ale1 = random.randint(0, 40)
+        for j in range(ale1):
+            filho1[j] = pais[len(pais)-i-1][j]
+        
+        for j in range(ale1, 40):
+            filho2[j] = pais[len(pais)-i-1][j]
  
-        filhos.append(filho)
+        ale2 = random.randint(0, 40)
+        for j in range(ale2):
+            filho3[j] = pais[len(pais)-i-1][j]
+        
+        for j in range(ale2, 40):
+            filho4[j] = pais[len(pais)-i-1][j]
 
-        filho = []
-        for j in range(40):
-            p_i = ajustar_ordem(pais[i][j], pais[len(pais)-i-1][j])
-            filho.append( random.uniform(p_i[0], p_i[1]) )
- 
-        filhos.append(filho)
-
-        filho = []
-        for j in range(40):
-            p_i = ajustar_ordem(pais[i][j], pais[len(pais)-i-1][j])
-            filho.append( random.uniform(p_i[0], p_i[1]) )
- 
-        filhos.append(filho)
+        filhos.append(filho1)
+        filhos.append(filho2)
+        filhos.append(filho3)
+        filhos.append(filho4)
 
     return filhos
 
 def mutacao_uniforme(pop, min, max):
-    sigma = 0.2
+    sigma = 2
     for i in range(len(pop)):
         for j in range(40):
             if(random.randint(1,50) == 1):
-                #print('.')
                 pop[i][j] += sigma*(max[j]-min[j])*(2*random.uniform(0,1)-1)
     
     return pop
 
 def restricao(somatorio_p_i):
     return (somatorio_p_i - 10500)
-
-def check_restricao(r):
-    if r <= 0 and r >= -1:
-        return 0
-    if r > 0:
-        return 1
-    
-    return -1
 
 def regras_factibilidade(filhos, min, dados_objetivo):
 
@@ -163,9 +142,9 @@ def regras_factibilidade(filhos, min, dados_objetivo):
         r1 = restricao(sum(ind1))
         r2 = restricao(sum(ind2))
 
-        fac_ind1 = check_restricao(r1)
+        fac_ind1 = abs(r1)
 
-        fac_ind2 = check_restricao(r2)
+        fac_ind2 = abs(r2)
 
         if(fac_ind1 == 0 and fac_ind2 == 0):
             if(fit1 >= fit2): # aquele que tiver menor valor da FO é o desejado
@@ -194,37 +173,26 @@ def fitness(pop, min, dados_objetivo):
     return av
 
 # Seleção para Sobrevivência
-def selecao_estacionario(pais, filhos, min, dados_objetivo):
+def selecao(pais, filhos):
     tam_pop = len(pais)
 
-    av_pais = fitness(pais, min, dados_objetivo)
-    pais_ordenados = [x for _, x in sorted(zip(av_pais, pais))]
+    pop = pais.copy()
 
-    av_filhos = fitness(filhos, min, dados_objetivo)
-    filhos_ordenados = [x for _, x in sorted(zip(av_filhos, filhos))]
-
-    t60 = math.floor(tam_pop*0.6)
-
-    pop = pais_ordenados.copy()
-    f = 0
-    for i in range(t60, tam_pop):
-        pop[i] = filhos_ordenados[f]
-        f += 1
+    for i in range(len(filhos)):
+        if(restricao(sum(filhos[i])) == 0):
+            pop[random.randint(0, tam_pop-1)] = filhos[i]
 
     return pop
 
-def atualiza_melhor(pais, melhor, min, dados_objetivo):
-    av_pais = fitness(pais, min, dados_objetivo)
-    pais_ordenados = [x for _, x in sorted(zip(av_pais, pais))]
+def atualiza_melhor(pop, melhor, min, dados_objetivo):
 
-    for j in range(len(pais_ordenados)):
-        fit_pai = calcular_funcao_individuo(pais_ordenados[j], min, dados_objetivo)
-        print("Comparacao: ", fit_pai, " -/- ", calcular_funcao_individuo(melhor, min, dados_objetivo))
-        print("Solucao analisada, result rest: ", abs(restricao(sum(pais_ordenados[j]))))
+    for j in range(len(pop)):
+        print("P: ", calcular_funcao_individuo(pop[j], min, dados_objetivo))
         
-        if(abs(restricao(sum(pais_ordenados[j]))) <= 50 and fit_pai < calcular_funcao_individuo(melhor, min, dados_objetivo) ):
-                    
-            melhor = pais_ordenados[j]
+        if(restricao(sum(pop[j])) == 0 and
+            calcular_funcao_individuo(pop[j], min, dados_objetivo) < calcular_funcao_individuo(melhor, min, dados_objetivo)):
+            melhor = pop[j]
+            print("M: ", calcular_funcao_individuo(melhor, min, dados_objetivo))
 
     return melhor
 
@@ -232,10 +200,8 @@ def algoritmo_genetico():
     min, max = retornar_intervalos()
     dados_objetivo = retornar_dados_objetivo()
 
-    print("MIN: ", sum(min), "Valor Fo: ",calcular_funcao_individuo(min, min, dados_objetivo))
-    print("MAX: ", sum(max), "Valor Fo: ",calcular_funcao_individuo(max, min, dados_objetivo))
     tam_pop = 10
-    n_geracoes = 30 # Número de gerações sem melhoria para parar a execução do método
+    n_geracoes = 20 # Número de gerações sem melhoria para parar a execução do método
 
     cp = 0
     
@@ -245,39 +211,36 @@ def algoritmo_genetico():
     melhor = pop[random.randint(0, len(pop)-1)]
 
     print("Checking: ", calcular_funcao_individuo(melhor, min, dados_objetivo))
-
+    print("SMI: ", sum(melhor))
 
     while True:
         pais = []
 
         # Seleção para Cruzamento
-        for i in range(tam_pop): pais.append(selecao_torneio(pop, 3, min, dados_objetivo))
+        for i in range(tam_pop): pais.append(selecao_torneio(pop, 2, min, dados_objetivo))
 
         # Cruzamento (Gera o dobro de filhos se comparado ao número de pais)
-        filhos = cruzamento_flat(pais)
-
+        filhos = cruzamento_simples(pais.copy())
 
         # Mutação
-        filhos = mutacao_uniforme(filhos, min, max)
+        filhos = mutacao_uniforme(filhos.copy(), min, max)
 
-
-        filhos = regras_factibilidade(filhos, min, dados_objetivo)
+        filhos = regras_factibilidade(filhos.copy(), min, dados_objetivo)
 
         # Seleção para Sobrevivência
-        pop = selecao_estacionario(pais, filhos, min, dados_objetivo)
-
+        pop = selecao(pop.copy(), filhos.copy())
 
         # Atualiza o melhor indivíduo
-        novo_melhor = atualiza_melhor(pop, melhor, min, dados_objetivo)
-        print("Valor: ",calcular_funcao_individuo(novo_melhor, min, dados_objetivo),  " Soma Pi: ", sum(novo_melhor), " Geração: ", cp)
+        novo_melhor = atualiza_melhor(pop.copy(), melhor, min, dados_objetivo)
+        print("Valor_NM: ",calcular_funcao_individuo(novo_melhor, min, dados_objetivo),  " Soma Pi: ", sum(novo_melhor), " Geração: ", cp)
         if(novo_melhor != melhor):
             melhor = novo_melhor
-            cp = 0
+            cp +=1
         else: cp += 1
 
         # Critério de Parada
         if(cp == n_geracoes):
-            #print(melhor)
+            print(melhor)
             print(calcular_funcao_individuo(melhor, min, dados_objetivo), " ", sum(melhor))
             break
             #return melhor, funcao(melhor[0], melhor[1])
